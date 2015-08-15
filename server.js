286088abbcb3484f870af9c93df5ee20474db61e
JayChetty <jaypaulchetty@gmail.com>
2015-08-15 19:28:41 +0100

    dangerous ability to delete all dbs and users, dev db option

--- server.js
+++ server.js
@@ -8,13 +8,18 @@ var nano = require('nano');
 
 server.listen(process.env.PORT || 8080);
 
+
+var devDb = 'http://admin:jakamama@localhost:5984/'
+var remoteDb = 'http://admin:jakamama@jakamama.iriscouch.com/';
+
+
 app.get('/add_group', function (req, res) {
   //should probably check user session key to make sure logged in
   var url = require('url');
   var url_parts = url.parse(req.url, true);
   var query = url_parts.query;
   console.log("asked for a GET", query)
-  var nano = require('nano')('http://admin:jakamama@jakamama.iriscouch.com/');
+  var nano = require('nano')(devDb);
   //creating new db for group
   nano.db.create(query.name, function(err, body) {
     if (!err) {
@@ -57,7 +62,7 @@ app.get('/add_user', function (req, res) {
   var url_parts = url.parse(req.url, true);
   var query = url_parts.query;
   console.log("asked for a GET", query)
-  var nano = require('nano')('http://admin:jakamama@jakamama.iriscouch.com/');
+  var nano = require('nano')(devDb);
   //creating new db for group
   res.set('Access-Control-Allow-Origin', '*');
   nano.db.create(query.name, function(err, body) {
@@ -74,6 +79,35 @@ app.get('/add_user', function (req, res) {
 
 app.get('/test', function (req, res) {
   res.status(200).send('Yay!');
+});
+
+app.get('/delete_all_dbs', function (req, res) {
+  var nano = require('nano')(devDb);
+  nano.db.list(function(err, body) {
+    // body is an array
+    body.forEach( function(db) {
+      // console.log(db);
+      if(db[0] != "_"){
+        nano.db.destroy(db)
+      }
+
+      // nano.db.destroy(db.name)
+    });
+  });  
+});
+
+app.get('/delete_all_users', function (req, res) {
+  var nano = require('nano')(devDb);
+  var db = nano.db.use('_users')
+  db.list(function(err, body) {
+    console.log('body.rows', body.rows)
+    body.rows.forEach(function(item){
+      if( item.id != "org.couchdb.user:admin" && item.id != "_design/_auth" ){
+        db.destroy(item.id, item.value.rev)
+        // console.log('item.id', item.id)
+      }
+    })      
+  }); 
 });
 
 //http://localhost:8080/?name=lala&user=jay
